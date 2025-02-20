18e5832ca00bb7703241e741e655cb746dc9f7b5
simarjeet <simarjeet.sm7@gmail.com>
2025-02-21 02:07:16 +0530

    hiterhe

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,12 +6,12 @@ conf();
 export const config = {
     port :process.env.PORT, 
     node_env:process.env.NODE_ENV, 
-    dbUrl :process.env.DATABASE_URL, 
-    jwtSecret:process.env.JWT_SECRET, 
-    geminiKey:process.env.GEMINI_API_KEY, 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    dbUrl :"mongodb+srv://simarjeetsinghmehrame21b1631:ZO2UAQ6NuL2lJS1m@assignment.uzor9.mongodb.net/assignmentdb?retryWrites=true&w=majority&appName=assignment", 
+    jwtSecret:"dfafasfasdf", 
+    geminiKey:"AIzaSyAN41IgOFv84WQA5Uh5Ic7ikJEAF5xmbK8", 
+    cloud_name: 'dliwrmlyb', 
+    api_key: '147124751375812' , 
+    api_secret: 'JjDYWW-zt2ny827VbPW-mCGjs2o'
 }
 
 
