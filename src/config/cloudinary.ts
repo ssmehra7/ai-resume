993d475f04a36936655e4fbd09361b215f530342
simarjeet <simarjeet.sm7@gmail.com>
2025-02-21 00:59:45 +0530

    improve the env

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
+import { config } from '.';
 
 cloudinary.config({ 
-    cloud_name: 'dliwrmlyb', 
-    api_key: '147124751375812', 
-    api_secret: 'JjDYWW-zt2ny827VbPW-mCGjs2o' // Click 'View API Keys' above to copy your API secret
+    cloud_name: config.cloud_name, 
+    api_key: config.api_key,
+    api_secret: config.api_secret,// Click 'View API Keys' above to copy your API secret
 });
 
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,9 @@ export const config = {
     dbUrl :process.env.DATABASE_URL, 
     jwtSecret:process.env.JWT_SECRET, 
     geminiKey:process.env.GEMINI_API_KEY, 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 }
 
 
