78e31297b79e433555a4523795833043cf6e931a
simarjeet <simarjeet.sm7@gmail.com>
2025-02-21 13:42:17 +0530

    prismaclient update

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,23 @@
 
+// import { PrismaClient } from '@prisma/client'
+
+// export const prisma = new PrismaClient()
+
+
+
+
+
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['query', 'error', 'warn'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+})
 
-
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
